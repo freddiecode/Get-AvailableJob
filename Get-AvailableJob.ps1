36888b908ccd664a2121af204440d5b9024b7042
Freddie Christiansen <56001252+freddiecode@users.noreply.github.com>
2021-02-08 18:38:31 +0100

    Update Get-AvailableJob.ps1

--- Get-AvailableJob.ps1
+++ Get-AvailableJob.ps1
@@ -2,42 +2,43 @@ function Get-AvailableJob {
 <#
 .SYNOPSIS
 
-Find out next job using PowerShell!
+Seach and find your next job using PowerShell!
 
 .DESCRIPTION
 
-Allows you to search for avaiable jobs via the NAV job ads public API.
+Allows you to search for available jobs via the NAV job ads public API.
 More information about the API is found here: https://github.com/navikt/pam-public-feed
 
-.PARAMETER Name
-Specifies the file name.
+.PARAMETER County
+Specifies the county in your search
 
-.PARAMETER Extension
-Specifies the extension. "Txt" is the default.
+.PARAMETER Municipal
+Specifies the municipality of your search.
+Remember that the municipality must be located in the County to get a valid request
 
-.INPUTS
-
-None. You cannot pipe objects to Add-Extension.
+.PARAMETER Category
+Specifies the job category of your search
+All categories are retrieved by the help of 'Dynamic Parameters' from and API endpoint at NAV
 
 .OUTPUTS
 
-System.String. Add-Extension returns a string with the extension
-or file name.
+TypeName: Selected.System.Management.Automation.PSCustomObject. Get-AvailableJob returns a PSCustomObject.
 
 .EXAMPLE
 
 Get-AvailableJob -County Innlandet -Municipal Hamar  -Category 'Helse og sosial' -ShowDescription
 
+.EXAMPLE
+
 Get-AvailableJob -County Innlandet -Municipal Lillehammer -Category IT
 
-Get-AvailableJob -County Oslo -Municipal Oslo -Category IT | Select-Object -First 10
+.EXAMPLE
 
+Get-AvailableJob -County Oslo -Municipal Oslo -Category IT | Select-Object -First 10
 
 .LINK
 
 https://www.cloudpilot.no
-
-
 #>
 
         [cmdletbinding()]
